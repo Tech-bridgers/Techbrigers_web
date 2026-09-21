858ed049fbcad63bcc5e0ce522f6cce847338986
Adesina Yusuf <adexyuslove@gmail.com>
2026-09-21 12:38:56 -0700

    fix: update max-width of Container component for better responsiveness

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -6,7 +6,7 @@ export function Container({
   children: React.ReactNode;
 }) {
   return (
-    <div className={`mx-auto w-full max-w-[1280px] px-6 sm:px-8 lg:px-11 ${className}`}>
+    <div className={`mx-auto w-full max-w-7xl px-6 sm:px-8 lg:px-11 ${className}`}>
       {children}
     </div>
   );
